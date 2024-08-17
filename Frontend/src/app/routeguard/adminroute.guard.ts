7df6a59408dba286a6e0a35f99dc0b9b612eeb17
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-08-17 11:16:13 +0530

    font fix

--- Frontend/src/app/routeguard/adminroute.guard.ts
+++ Frontend/src/app/routeguard/adminroute.guard.ts
@@ -6,51 +6,36 @@ export const adminrouteGuard: CanActivateFn = async (route, state) => {
   const router = inject(Router);
   const database = inject(DatabaseService);
 
-  var promise = new Promise((res) => {
-    // if (typeof localStorage !== 'undefined') {
-    if (localStorage.getItem('token') !== 'undefined') {
-      if (localStorage.getItem('token')) {
-        database.postVerifyToken(localStorage.getItem('token')).subscribe((data: any) => {
-          console.log('admin route gulared', data)
-          if (data.status === true) {
-            console.log('deepak in the admin router ')
-            res(true)
-          } else {
-            localStorage.removeItem('token');
-            localStorage.removeItem('userType');
-            localStorage.removeItem('user');
-            localStorage.removeItem('loginStatus');
-            res(false)
-          }
-        })
-      } else {
-        localStorage.removeItem('token');
-        localStorage.removeItem('userType');
-        localStorage.removeItem('user');
-        localStorage.removeItem('loginStatus');
-        res(false)
+  if (typeof window !== 'undefined' && localStorage.getItem('token')) {
+    const token = localStorage.getItem('token');
+
+    const promise = new Promise<boolean>((resolve) => {
+      database.postVerifyToken(token).subscribe((data: any) => {
+        if (data.status === true) {
+          resolve(true);
+        } else {
+          clearLocalStorage();
+          resolve(false);
+        }
+      });
+    });
+
+    const isVerified = await promise;
+    if (isVerified) {
+      if (localStorage.getItem('userType') === 'isAdmin') {
+        return true;
       }
     }
-  })
-
-  var check = await promise
-  if (check === true) {
-    if (localStorage.getItem('userType') === 'isAdmin') {
-      return true
-    } else {
-      router.navigateByUrl('/login')
-      localStorage.removeItem('token')
-      localStorage.removeItem('userType');
-      localStorage.removeItem('user');
-      localStorage.removeItem('loginStatus')
-      return false
-    }
-  } else {
-    router.navigateByUrl('/login');
-    localStorage.removeItem('token');
-    localStorage.removeItem('userType');
-    localStorage.removeItem('user');
-    localStorage.removeItem('loginStatus')
-    return false
   }
+
+  router.navigateByUrl('/login');
+  clearLocalStorage();
+  return false;
 };
+
+function clearLocalStorage() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('userType');
+  localStorage.removeItem('user');
+  localStorage.removeItem('loginStatus');
+}
