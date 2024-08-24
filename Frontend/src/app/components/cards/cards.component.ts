8ef84df8293490d5d1b85ddca9c834478ed1f508
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-08-24 14:57:28 +0530

    card images render fix

--- Frontend/src/app/components/cards/cards.component.ts
+++ Frontend/src/app/components/cards/cards.component.ts
@@ -1,35 +1,41 @@
-import { Component ,Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Properties } from '../../Models/properties';
 import { DatabaseService } from '../../services/database.service';
-
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css'] 
 })
-export class CardsComponent implements OnInit{
-  @Input() properties:Properties[] | undefined;
+export class CardsComponent implements OnInit {
+  @Input() properties: Properties[] | undefined;
 
-constructor(private dbServ: DatabaseService){}
+  constructor(private dbServ: DatabaseService) {}
 
-ngOnInit(): void {
+  ngOnInit(): void {
     this.getAllProperties();
-    this.setPropertyType();
-}
+  }
 
   getAllProperties() {
-    this.dbServ.getAllProperties().subscribe((res)=>{
-        this.properties = res
-        .filter((element)=> element.isApproved == 1)
-    })
+    this.dbServ.getAllProperties().subscribe((res) => {
+      this.properties = res.filter((element) => element.isApproved === 1);
+      this.setPropertyType();
+    });
   }
 
-  setPropertyType(){
-    this.properties?.forEach((property)=>{
-      if(property.propertyType == "1") property.propertyType = "Residential Buy"
-      if(property.propertyType == "2") property.propertyType = "Residential Rent"
-    })
+  setPropertyType() {
+    if (this.properties) {
+      this.properties.forEach((property) => {
+        if (property.propertyType === "1") {
+          property.propertyType = "Residential Buy";
+        } else if (property.propertyType === "2") {
+          property.propertyType = "Residential Rent";
+        }else if (property.propertyType === "3") {
+          property.propertyType = "Commercial Buy";
+        }else if (property.propertyType === "4") {
+          property.propertyType = "Commercial Rent";
+        }
+      });
+    }
   }
-
 }
