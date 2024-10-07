94bff51ba7fc496b1b33ebd0110222ec54fd96d9
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-10-07 18:02:22 +0530

    folder changes

--- frontend-react/src/components/Layout.tsx
+++ frontend-react/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
-import Navbar  from './Pages/Home/Navbar'
-import Footer from './Pages/Home/Footer'
+import Navbar  from './Navbar'
+import Footer from './Footer'
 
 function Layout() {
     return (
