38f72765d1e25bc192d44ed6642a7a0809cfd6ad
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-10-07 18:09:46 +0530

    minor changes

--- frontend-react/src/components/Pages/Dashboard/Sidebar.tsx
+++ frontend-react/src/components/Pages/Dashboard/Sidebar.tsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import {
   Home,
   LayoutDashboard,
-  Settings,
-  ShoppingCart,
   Users,
   Menu,
   User,
