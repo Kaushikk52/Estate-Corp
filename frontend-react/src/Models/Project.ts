b5ab5432f70fa559d7e82873b83e4879b29f061d
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-10-08 07:49:48 +0530

    Project model changes

--- frontend-react/src/Models/Project.ts
+++ frontend-react/src/Models/Project.ts
@@ -1,8 +1,17 @@
+import Address from "./Address";
+import FloorPlan from "./FloorPlan";
 interface Project {
     id: string;
     name: string;
+    description: string;
     images: string[];
-
+    totalFloor: number;
+    location: string;
+    builtIn: string;
+    possesion: string;
+    ammenities: string[];
+    address: Address;
+    floorPlans: FloorPlan[];
 }
 
 export default Project;
