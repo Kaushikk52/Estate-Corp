f38a4618c39fb28ae31fb9be8f45af071c674492
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-10-08 07:52:53 +0530

    FLoor plan model added

---
+++ frontend-react/src/Models/FloorPlan.ts
@@ -0,0 +1,15 @@
+interface FloorPlan {
+    id: string;
+    name: string;
+    bedrooms: number;
+    bathrooms: number;
+    balconies: number;
+    price: number;
+    priceUnit: string;
+    carpetArea: number;
+    areaUnit: string;
+    description: string;
+    image: string;
+}
+
+export default FloorPlan;
