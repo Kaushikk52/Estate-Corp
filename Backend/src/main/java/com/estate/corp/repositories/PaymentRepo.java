41487fa6412e92714b26127a51165b7aeac25da9
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-10-10 00:09:39 +0530

    removed unnessary modules

--- Backend/src/main/java/com/estate/corp/repositories/PaymentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.estate.corp.repositories;
-
-import com.estate.corp.models.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepo extends JpaRepository<Payment,String> {
-}
