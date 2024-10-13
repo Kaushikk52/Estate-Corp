7a939b1fa72489056c04288aaed36d438589c14d
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-10-13 16:52:16 +0530

    carousel changed

--- frontend-react/src/components/Pages/Home/Home.tsx
+++ frontend-react/src/components/Pages/Home/Home.tsx
@@ -40,13 +40,13 @@ function Home() {
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 mt-5">
         <div className="relative h-[200px] sm:h-[300px] md:h-[400px] mb-5 rounded-xl overflow-hidden bg-[#d0d9e6]">
           <img 
-            src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" 
+            src="/carousel3.jpg" 
             alt="Modern furniture" 
             className="w-full h-full object-cover object-center"
           />
           <div className="absolute inset-0 flex items-center justify-center">
             <motion.h1 
-              className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white text-center leading-tight drop-shadow-lg text-opacity-80"
+              className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-slate-800 text-center leading-tight drop-shadow-lg text-opacity-80"
               variants={containerVariants}
               initial="hidden"
               animate="visible"
