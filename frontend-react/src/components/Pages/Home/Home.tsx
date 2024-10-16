e07fe5cf5845706bc5bc74f3fd356d79606c5943
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-10-16 10:43:39 +0530

    carousel slide text opacity changed

--- frontend-react/src/components/Pages/Home/Home.tsx
+++ frontend-react/src/components/Pages/Home/Home.tsx
@@ -46,7 +46,7 @@ function Home() {
           />
           <div className="absolute inset-0 flex items-center justify-center">
             <motion.h1 
-              className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-slate-800 text-center leading-tight drop-shadow-lg text-opacity-70"
+              className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-slate-950 text-center leading-tight drop-shadow-lg text-opacity-70"
               variants={containerVariants}
               initial="hidden"
               animate="visible"
