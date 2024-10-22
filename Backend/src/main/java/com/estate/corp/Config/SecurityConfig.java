838989906d577caf949082dbaaf07db69a67f57a
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-10-22 16:06:01 +0530

    security sync test

--- Backend/src/main/java/com/estate/corp/Config/SecurityConfig.java
+++ Backend/src/main/java/com/estate/corp/Config/SecurityConfig.java
@@ -40,27 +40,28 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v1/api/auth/*").permitAll()
+                        // Authentication endpoints
+                        .requestMatchers(HttpMethod.POST, "/v1/api/auth/**").permitAll()
+
+                        // Users endpoints
                         .requestMatchers(HttpMethod.GET, "/v1/api/users/all").hasRole("ADMIN")
-                        .requestMatchers("/v1/api/users/**").authenticated()
-                        .requestMatchers(HttpMethod.PUT, "/v1/api/users/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/v1/api/users/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE,
-                                "/v1/api/users/removeProject/*").hasAnyRole("ADMIN", "AGENT")
-                        .requestMatchers(HttpMethod.DELETE,
-                                "/v1/api/users/removeProperty/*").hasAnyRole("ADMIN", "AGENT", "RESALER")
+                        .requestMatchers(HttpMethod.GET, "/v1/api/users/adminProperties").permitAll()
+                        .requestMatchers("/v1/api/users/**").authenticated() // Applies to all HTTP methods
+                        .requestMatchers(HttpMethod.DELETE, "/v1/api/users/removeProject/*").hasAnyRole("ADMIN", "AGENT")
+                        .requestMatchers(HttpMethod.DELETE, "/v1/api/users/removeProperty/*").hasAnyRole("ADMIN", "AGENT", "RESALER")
+
+                        // Projects endpoints
                         .requestMatchers("/v1/api/projects/add").hasAnyRole("ADMIN", "AGENT")
-                        .requestMatchers(HttpMethod.GET, "/v1/api/projects/all",
-                                "/v1/api/projects/id/*",
-                                "/v1/api/projects/name/*").authenticated()
-                        .requestMatchers(HttpMethod.GET, "/v1/api/properties/filter",
-                                "/v1/api/properties/all",
-                                "/v1/api/properties/isApproved",
-                                "/v1/api/properties/id/*",
-                                "/v1/api/properties/name/*").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/api/projects/all","/v1/api/projects/filter", "/v1/api/projects/id/*", "/v1/api/projects/name/*").permitAll()
+
+                        // Properties endpoints
+                        .requestMatchers(HttpMethod.GET, "/v1/api/properties/filter", "/v1/api/properties/all", "/v1/api/properties/isApproved", "/v1/api/properties/id/*", "/v1/api/properties/name/*").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/v1/api/properties/approvalStatus/*").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST,
-                                "/v1/api/properties/post").hasAnyRole("ADMIN", "AGENT", "RESALER")
+                        .requestMatchers(HttpMethod.POST, "/v1/api/properties/post").authenticated()
+
+                        //Notifications endpoints
+                        .requestMatchers("/v1/api/enquiry/email","/v1/api/enquiry/all").permitAll()
+
                 )
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -75,7 +76,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(frontendUrl));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type")); // Add allowed headers if needed
         configuration.setAllowCredentials(true); // Allow credentials if needed
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
