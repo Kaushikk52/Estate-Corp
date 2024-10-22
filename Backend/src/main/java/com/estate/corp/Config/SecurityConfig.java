29ef35307338f3860ce636a6e453e7a768dc0abf
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-10-23 00:32:53 +0530

    config changes

--- Backend/src/main/java/com/estate/corp/Config/SecurityConfig.java
+++ Backend/src/main/java/com/estate/corp/Config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(frontendUrl));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
